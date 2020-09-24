f096e1a56a993919862a43c5e370a010142f6613
Alex <alex.tan@ubtrobot.com>
2020-09-24 13:55:07 +0800

    heap

---
+++ week09/heap/heap.js
@@ -0,0 +1,72 @@
+function parent(s) {
+  return s % 2 === 0 ? parseInt(s / 2 - 1) : parseInt((s - 1) / 2);
+}
+
+function left(i) {
+  return i * 2 + 1;
+}
+
+function right(i) {
+  return i * 2 + 2;
+}
+
+class MinHeap {
+  constructor(arr, compare) {
+    this.data = arr.slice();
+    this.compare = compare || ((a, b) => a > b);
+    const pLen = parseInt(this.data.length / 2, 10);
+    for (let s = pLen - 1; s >= 0; s--) {
+      this.shiftDown(s);
+    }
+  }
+
+  get length() {
+    return this.data.length;
+  }
+
+  swap(a, b) {
+    let tmp = this.data[a];
+    this.data[a] = this.data[b];
+    this.data[b] = tmp;
+  }
+
+  shiftUp(s) {
+    const d = this.data;
+    while (s > 0 && this.compare(d[parent(s)], d[s])) {
+      this.swap(s, parent(s));
+      s = parent(s);
+    }
+  }
+  
+  shiftDown(s) {
+    const d = this.data;
+    while (left(s) <= d.length) {
+      const minChildIdx = this.compare(d[left(s)], d[right(s)]) ? right(s) : left(s);
+      if (this.compare(d[s], d[minChildIdx])) {
+        this.swap(s, minChildIdx);
+      }
+      s = minChildIdx;
+    }
+  }
+
+  take(index=0) {
+    this.swap(index, this.data.length - 1);
+    const tmp = this.data.pop();
+    this.shiftDown(index);
+    return tmp;
+  }
+  
+  give(value) {
+    this.data.push(value);
+    this.shiftUp(this.data.length - 1);
+  }
+}
+
+// (function () {
+//   const arr = [7, 1, 3, 10, 5, 2, 8, 9, 6, 4];
+
+//   const heap = new MinHeap(arr);
+//   heap.give(0);
+//   console.log(heap.take());
+// })();
+
---
+++ week09/heap/sorted.js
@@ -0,0 +1,42 @@
+
+class Sorted {
+  constructor(data, compare) {
+    this.data = data.slice();
+    this.compare = compare || ((a, b) => a > b);
+  }
+
+  get length() {
+    return this.data.length;
+  }
+
+  take() {
+    if (!this.data.length) {
+      return;
+    }
+    let min = this.data[0];
+    let minIndex = 0;
+
+    for (let i = 0; i < this.data.length; i++) {
+      if (!this.compare(this.data[i], min)) {
+        min = this.data[i];
+        minIndex = i;
+      }
+    }
+
+    this.data[minIndex] = this.data[this.data.length - 1];
+    this.data.pop();
+    return min;
+  }
+
+  give(v) {
+    this.data.push(v);
+  }
+}
+
+(function () {
+  const arr = [7, 1, 3, 10, 5, 2, 8, 9, 6, 4];
+
+  const s = new Sorted(arr);
+  s.give(0);
+  console.log(s.take());
+})();
